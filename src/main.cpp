4978fe1c509cf4834124aeb915f8cde55ca46ecd
txema <txedav@gmail.com>
2025-10-26 00:22:32 +0200

    Concurrent classic async implementation

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
  * - We start with a single thread io operations processing
  *
  */
+#include "threadpool.hpp"
+
 #include <fcntl.h>
 #include <filesystem>
 #include <print>
 #include <string>
 #include <string_view>
 #include <unistd.h>
+#include <unordered_set>
 #include <variant>
 #include <vector>
 
@@ -88,6 +91,79 @@ bool readFileHasValidNumberOfDigits(const fs::path& path)
     return count % 10 == 0;
 }
 
+std::future<size_t> countNumbersInFileAsync(const fs::path& path, ThreadPool& threadPool)
+{
+    std::promise<size_t> promise;
+    std::future<size_t> future = promise.get_future();
+    if (!fs::exists(path))
+    {
+        promise.set_value(0);
+        return future;
+    }
+
+    const auto fd = open(path.c_str(), O_RDONLY);
+    if (fd == -1)
+    {
+        promise.set_value(0);
+        return future;
+    }
+    threadPool.enqueue(
+        [fd, prom = std::move(promise)]() mutable
+        {
+            size_t count = 0;
+            char buffer[4096];
+            ssize_t bytesRead;
+            while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0)
+            {
+                for (ssize_t i = 0; i < bytesRead; ++i)
+                {
+                    if (buffer[i] >= '0' && buffer[i] <= '9')
+                    {
+                        ++count;
+                    }
+                }
+            }
+            prom.set_value(count);
+        });
+
+    return std::async(std::launch::deferred,
+                      [fd, future = std::move(future)]() mutable -> size_t
+                      {
+                          future.wait();
+                          if (close(fd) == -1)
+                          {
+                              return 0;
+                          }
+                          return future.get();
+                      });
+}
+
+std::future<bool> readFileHasValidNumberOfDigitsAsync(const fs::path& path, ThreadPool& threadPool)
+{
+    std::future<size_t> countFut = countNumbersInFileAsync(path, threadPool);
+    return std::async(std::launch::deferred,
+                      [cf = std::move(countFut)]() mutable
+                      {
+                          size_t count = cf.get();
+                          return count % 10 == 0;
+                      });
+}
+
+bool isValidNumberCount(size_t count)
+{
+    return count % 10 == 0;
+}
+
+std::future<bool> isValidNumberCount(std::future<size_t> countFuture)
+{
+    return std::async(std::launch::deferred,
+                      [cf = std::move(countFuture)]() mutable
+                      {
+                          size_t count = cf.get();
+                          return count % 10 == 0;
+                      });
+}
+
 bool writeToFile(const fs::path& path, std::string_view data)
 {
     // Open the file using linux api
@@ -112,13 +188,16 @@ struct overloaded: Ts...
     using Ts::operator()...;
 };
 
-bool processOperation(const Operation& op)
+using ResultType = std::variant<bool, std::future<bool>>;
+
+ResultType processOperation(const Operation& op, ThreadPool& threadPool)
 {
-    return std::visit(overloaded{[](const ReadOperation& readOp) -> bool
+    return std::visit(overloaded{[&threadPool](const ReadOperation& readOp) -> ResultType
                                  {
-                                     return readFileHasValidNumberOfDigits(readOp.path);
+                                     return readFileHasValidNumberOfDigitsAsync(readOp.path,
+                                                                                threadPool);
                                  },
-                                 [](const WriteOperation& writeOp) -> bool
+                                 [](const WriteOperation& writeOp) -> ResultType
                                  {
                                      return writeToFile(writeOp.path, writeOp.data);
                                  }},
@@ -159,23 +238,47 @@ public:
 
     void runIteration()
     {
-        for (auto it = mOperations.begin(); it != mOperations.end();)
+        std::vector<ResultType> results;
+        results.reserve(mOperations.size());
+        for (size_t i = 0; i < mOperations.size(); ++i)
         {
-            bool remove = processOperation(*it);
-            if (remove)
-            {
-                it = mOperations.erase(it);
-            }
-            else
-            {
-                ++it;
-            }
+            results.push_back(processOperation(mOperations[i], mThreadPool));
         }
+        std::unordered_set<size_t> indicesToErase;
+        indicesToErase.reserve(mOperations.size());
+        for (size_t i = 0; i < results.size(); ++i)
+        {
+            std::visit(overloaded{[&indicesToErase, i](bool remove)
+                                  {
+                                      if (remove)
+                                      {
+                                          indicesToErase.insert(i);
+                                      }
+                                  },
+                                  [&indicesToErase, i](std::future<bool> removeFut)
+                                  {
+                                      if (removeFut.get())
+                                      {
+                                          indicesToErase.insert(i);
+                                      }
+                                  }},
+                       std::move(results[i]));
+        }
+        size_t i = 0;
+        mOperations.erase(std::remove_if(mOperations.begin(),
+                                         mOperations.end(),
+                                         [&indicesToErase, &i](const Operation&)
+                                         {
+                                             return indicesToErase.contains(i++);
+                                         }),
+                          mOperations.end());
     }
 
 private:
     std::vector<Operation> mOperations;
     const std::string mBuffer = generateRandomString(5 * 1024 * 1024);
+
+    ThreadPool mThreadPool{4};
 };
 
 int main()
