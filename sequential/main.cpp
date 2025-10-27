e6a353b910e1756ca06b4a2025368a2be5b8b47d
txema <txedav@gmail.com>
2025-10-27 23:42:51 +0100

    Seq: Implement write in chunks operation

--- sequential/main.cpp
+++ sequential/main.cpp
@@ -36,19 +36,33 @@ struct WriteOperation
     std::string_view data;
 };
 
-using Operation = std::variant<ReadOperation, WriteOperation>;
+struct WriteInChunksOperation
+{
+    fs::path path;
+    std::string_view data;
+    size_t chunkSize;
+};
+
+using Operation = std::variant<ReadOperation, WriteOperation, WriteInChunksOperation>;
 
 Operation CreateRandomOperation(const std::string& buffer)
 {
-    if (rand() % 2 == 0)
+    const auto dice = rand() % 3;
+    const fs::path filePath{"file_" + std::to_string(rand() % 100) + ".txt"};
+    if (dice == 0)
     {
-        return ReadOperation{fs::path("file_" + std::to_string(rand() % 100) + ".txt")};
+        return ReadOperation{filePath};
+    }
+    const size_t start = static_cast<size_t>(rand()) % (buffer.size() - 1024 * 1024);
+    const std::string_view dataView{buffer.data() + start, 1024 * 1024};
+    if (dice == 1)
+    {
+        return WriteOperation{filePath, dataView};
     }
     else
     {
-        size_t start = static_cast<size_t>(rand()) % (buffer.size() - 1024 * 1024);
-        return WriteOperation{fs::path("file_" + std::to_string(rand() % 100) + ".txt"),
-                              std::string_view(buffer.data() + start, 1024)};
+        const size_t nChunks = static_cast<size_t>(rand()) % 5u + 5u;
+        return WriteInChunksOperation{filePath, dataView, nChunks};
     }
 }
 
@@ -56,6 +70,7 @@ size_t countNumbersInFile(const fs::path& path)
 {
     if (!fs::exists(path))
     {
+        std::println("countNumbersInFile: file {} does not exist", path.string());
         return 0;
     }
     // Open the file using linux api
@@ -63,6 +78,7 @@ size_t countNumbersInFile(const fs::path& path)
     const auto fd = open(path.c_str(), O_RDONLY);
     if (fd == -1)
     {
+        std::println("countNumbersInFile: open failed for file {}", path.string());
         return 0;
     }
     size_t count = 0;
@@ -88,22 +104,53 @@ bool readFileHasValidNumberOfDigits(const fs::path& path)
     return count % 10 == 0;
 }
 
-bool writeToFile(const fs::path& path, std::string_view data)
+bool writeToFile(const fs::path& path,
+                 std::string_view data,
+                 std::optional<off_t> offset = std::nullopt)
 {
     // Open the file using linux api
-    const auto fd = open(path.c_str(), O_WRONLY | O_CREAT | O_APPEND, 0644);
+    const auto fd = open(path.c_str(), O_WRONLY | O_CREAT | (offset ? 0 : O_APPEND), 0644);
     if (fd == -1)
     {
+        std::println("write: open failed");
         return false;
+    }
+    if (offset)
+    {
+        if (::lseek(fd, *offset, SEEK_SET) == (off_t)-1)
+        {
+            ::close(fd);
+            std::println("write: lseek failed");
+            return false;
+        }
     }
     ssize_t bytesWritten = write(fd, data.data(), data.size());
     if (bytesWritten == -1)
     {
+        std::println("write: write failed");
         close(fd);
         return false;
     }
     close(fd);
     return bytesWritten == static_cast<ssize_t>(data.size());
+}
+
+bool writeToFileInChunks(const fs::path& path, std::string_view data, size_t nChunks)
+{
+    size_t totalSize = data.size();
+    size_t offset = 0;
+    size_t chunkSize = totalSize / nChunks;
+    while (offset < totalSize)
+    {
+        size_t currentChunkSize = std::min(chunkSize, totalSize - offset);
+        if (!writeToFile(path, data.substr(offset, currentChunkSize), offset))
+        {
+            std::println("writeToFileInChunks: writeToFile failed at offset {}", offset);
+            return false;
+        }
+        offset += currentChunkSize;
+    }
+    return true;
 }
 
 template<class... Ts>
@@ -114,21 +161,26 @@ struct overloaded: Ts...
 
 bool processOperation(const Operation& op)
 {
-    return std::visit(overloaded{[](const ReadOperation& readOp) -> bool
-                                 {
-                                     return readFileHasValidNumberOfDigits(readOp.path);
-                                 },
-                                 [](const WriteOperation& writeOp) -> bool
-                                 {
-                                     return writeToFile(writeOp.path, writeOp.data);
-                                 }},
-                      op);
+    return std::visit(
+        overloaded{[](const ReadOperation& readOp) -> bool
+                   {
+                       return readFileHasValidNumberOfDigits(readOp.path);
+                   },
+                   [](const WriteOperation& writeOp) -> bool
+                   {
+                       return writeToFile(writeOp.path, writeOp.data);
+                   },
+                   [](const WriteInChunksOperation& writeOp) -> bool
+                   {
+                       return writeToFileInChunks(writeOp.path, writeOp.data, writeOp.chunkSize);
+                   }},
+        op);
 }
 
 class Component
 {
 public:
-    static constexpr size_t numOperations = 1000;
+    static constexpr size_t numOperations = 50;
 
     Component()
     {
@@ -143,6 +195,7 @@ public:
     {
         for (size_t i = 0; i < iterations; ++i)
         {
+            std::println("Event loop iteration {}", i + 1);
             runIteration();
             refillOperationsIfNeeded();
         }
@@ -181,7 +234,7 @@ int main()
 {
     Component component{};
     std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-    component.eventLoop(20);
+    component.eventLoop(10);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     std::println("Execution time: {} ms", duration);
@@ -197,4 +250,3 @@ int main()
     }
     return 0;
 }
-
